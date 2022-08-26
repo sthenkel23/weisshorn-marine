41eac8144fb962d10b34f0d715449e3b47680231
SteffEn Henkelmann <40469614+sthenkel23@users.noreply.github.com>
2022-08-26 19:02:31 +0300

    initial setup for app

---
+++ Makefile
@@ -0,0 +1,12 @@
+install:
+	pip install --upgrade pip && \
+		pip install -r requirements.txt
+
+create-virtual:
+	python3 -m venv ~/.env
+
+source-virtual:
+	source ~/.env/bin/activate
+
+run-app:
+	streamlit run src/marine/app/app.py
---
+++ src/marine/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from summa import summarizer
+import streamlit as st
+
+# Add title to the page.
+st.title("Text summarization")
+
+# Ask user for input text.
+input_sent = st.text_area("Input Text", "", height=400)
+
+# User input on what fraction of the original text to return.
+ratio = st.slider(
+    "Summarization fraction", min_value=0.0, max_value=1.0, value=0.2, step=0.01
+)
+
+# Summarize the original text.
+summarized_text = summarizer.summarize(
+    input_sent, ratio=ratio, language="english", split=True, scores=True
+)
+
+# Print out the results.
+for sentence, score in summarized_text:
+    st.write(sentence)
