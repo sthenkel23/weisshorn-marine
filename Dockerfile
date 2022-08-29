f8415265adbe9765714e42436207d9d59be054f4
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-08-29 11:42:58 +0200

    g-cl

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ RUN pip3 install -r requirements.txt
 COPY src/dist/marine-0.0.0.1-py3-none-any.whl ./marine-0.0.0.1-py3-none-any.whl
 RUN pip3 install marine-0.0.0.1-py3-none-any.whl
 
-RUN echo FIREBASE_API_KEY > gcl.json
 COPY gcl.json ./gcl.json
 
 COPY src/app.py .
--- src/db/firestore.py
+++ src/db/firestore.py
@@ -1,8 +1,7 @@
 from google.cloud import firestore
 import os
-
 # Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json(os.environ["FIREBASE_API_KEY"])
+db = firestore.Client.from_service_account_json("./g-cl.json")
 
 # Create a reference to the Google post.
 doc_ref = db.collection("marine-alerts").document("alerts")
