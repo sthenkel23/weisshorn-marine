a678417cc57071ed13cf5f284f305a0c42e167ba
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-06 18:01:56 +0200

    send json

--- consumer/src/marine/utils.py
+++ consumer/src/marine/utils.py
@@ -10,12 +10,12 @@ if getenv("IS_DOCKERIZED"):
     WS_CONN = f"ws://{HEROKU_BACKEND_NAME}.herokuapp.com/sample"
     # WS_CONN = "ws://weisshorn-backend.herokuapp.com/sample"
 else:
-    WS_CONN = "ws://0.0.0.0:8000/sample"
+    WS_CONN = "ws://127.0.0.1:8000/sample"
 
 
 async def consumer(graphs, window_size, status):
     windows = defaultdict(partial(deque, [0] * window_size, maxlen=window_size))
-    # twindows = defaultdict(partial(deque, [0] * window_size, maxlen=window_size))
+    twindows = defaultdict(partial(deque, [0] * window_size, maxlen=window_size))
 
     async with aiohttp.ClientSession(trust_env=True) as session:
         status.subheader(f"Connecting to {WS_CONN}")
@@ -25,16 +25,16 @@ async def consumer(graphs, window_size, status):
                 data = message.json()
 
                 windows[data["channel"]].append(data["data"])
-                # twindows[data["channel"]].append(data["amount"])
+                twindows[data["channel"]].append(data["amount"])
 
                 for channel, graph in graphs.items():
                     channel_data = {channel: windows[channel]}
-                    # channel_tdata = {channel: twindows[channel]}
+                    channel_tdata = {channel: twindows[channel]}
                     if channel == "A":
                         graph.line_chart(channel_data)
                     elif channel == "B":
                         graph.area_chart(channel_data)
                     elif channel == "C":
                         graph.bar_chart(channel_data)
-                    # elif channel == "D":
-                    #     graph.line_chart(channel_tdata)
+                    elif channel == "D":
+                        graph.line_chart(channel_tdata)
--- producer/src/data/api.py
+++ producer/src/data/api.py
@@ -10,5 +10,5 @@ def fetch_data_cb_api_continuously():
     """
     response = requests.get("https://api.coinbase.com/v2/prices/ETH-USD/spot")
     r = response.json()["data"]
-    r["timestamp"] = pd.to_datetime("today").now()
+    # r["timestamp"] = pd.to_datetime("today").now()
     return response, r
