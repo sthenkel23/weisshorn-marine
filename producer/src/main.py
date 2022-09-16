32d60a8e568228a1b38c6bfad110dcf5943e10b3
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-16 20:56:45 +0200

    up

--- producer/src/main.py
+++ producer/src/main.py
@@ -18,7 +18,10 @@ def index():
 
 @app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
 async def read_item(item_id: str):
-    return items[item_id]
+    if item_id == "dump":
+        return items
+    else:
+        return items[item_id]
 
 
 @app.get("/apiv1/{name}")
